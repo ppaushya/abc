ea4522a8ae33607f910b19a2d53698516e0cadb7
gbaser <gbaser@DIN73002996.corp.capgemini.com>
2018-11-23 14:58:20 +0530

    third

--- GitDemo/src/main/java/BootClass.java
+++ GitDemo/src/main/java/BootClass.java
@@ -3,7 +3,7 @@ public class BootClass {
 
 	public static void main(String[] args) {
 	System.out.println("Git Demo");
-	System.out.println("Thanks Akhil");
+	System.out.println("Thanks to all!!!!");
 
 	}
 
